9a023491895fa3194d6133e68da36390cc905c0e
malka-rozental <maliwein990@gmail.com>
2024-10-14 16:24:44 +0300

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { Box, Container, Stack } from "@mui/material";
 import "./App.css";
-import { ProductList } from "./components/productsList/ProductList";
-import { ProductAddEdit } from "./components/ProductAddEdit";
-import { useProduct } from "./components/ProductProvider";
 import { Header } from "./components/header/Header";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Home } from "./components/home";
 
 function App() {
-  const { addNewProduct, selectedProduct } = useProduct();
   return (
     <Container fixed>
       <Box sx={{ height: "100vh" }}>
         <Header />
-        <Stack
-          spacing={2}
-          direction="row"
-          sx={{ alignItems: "center", justifyContent: "space-between" }}
-        >
-          <ProductList />
-          {(addNewProduct || selectedProduct) && <ProductAddEdit />}
-        </Stack>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/product" />} />
+            <Route path="/product" element={<Home />} />
+          </Routes>
+        </Router>
       </Box>
     </Container>
   );
